ef163261da293902b1a23ef6aa5f4649df851792
Zeromika <umutguler97@gmail.com>
2019-04-24 13:56:19 +0300

    Minor Changes

--- process_activity.py
+++ process_activity.py
@@ -38,13 +38,16 @@ Base = declarative_base()
 Base.metadata.create_all(engine)
 connection = engine.connect()
 metadata = db.MetaData()
-anomalies_table = db.Table('DetectedAnomalies', metadata, autoload=True, autoload_with=engine)
-video_anomalies_table = db.Table('VideoDetectedAnomaly', metadata, autoload=True, autoload_with=engine)
+anomalies_table = db.Table('detected_anomalies', metadata, autoload=True, autoload_with=engine)
+video_anomalies_table = db.Table('video_detected_anomaly', metadata, autoload=True, autoload_with=engine)
 #Check with sql statement and get the whole stream of video.
-video_table = db.Table('Videos',metadata,autoload=True, autoload_with=engine)
+video_table = db.Table('videos',metadata,autoload=True, autoload_with=engine)
 
 video_details = connection.execute(db.select([video_table]).where(video_table._columns.VideoId == vid_id)).fetchall()[0]
-vid_name = video_details['Name']
+vid_name = video_details['name']
+vid_w = video_details['width']
+vid_h = video_details['height']
+relative_size = {"width": vid_w, "height": vid_h}
 # ffmpeg -i input -vf scale=iw/2:-1 output
 
 def exec_long_running_proc(command, args):
@@ -57,7 +60,7 @@ def exec_long_running_proc(command, args):
     while True:
         nextline = process.stdout.readline().decode('UTF-8')
         if 'JSON-stream sent.' in nextline and not thread1:
-            thread1 = threading.Thread(target = grabResults, args = (db, connection))
+            thread1 = threading.Thread(target = grabResults, args = (db, connection, relative_size))
             thread1.start()
             logging.warning("List of threads: " + str(threading.enumerate()))
         if nextline == '' and process.poll() is not None:
@@ -75,17 +78,24 @@ def exec_long_running_proc(command, args):
 
 
 
-def grabResults(db,connection):
+
+def grabResults(db,connection, relative_size):
+    progress = 0
     r = requests.get('https://api.myjson.com/bins/wrc88')
     processed_results = r.json()
     logging.warning("Started grabbing results")
     for result in processed_results:
         if len(result['objects']):
             for obj in result['objects']:
-                query = connection.execute(db.insert(anomalies_table).values(RuleId = 1,frame =result['frame_id'], center_x = obj['relative_coordinates']['center_x'], center_y = obj['relative_coordinates']['center_y'], width = obj['relative_coordinates']['width'], height = obj['relative_coordinates']['height']))
+                center_x = center_x * relative_size['width']
+                center_y = center_y * relative_size['height']
+                width = width * relative_size['width']
+                height = height * relative_size['height']
+                center_x - relative_size['width']/2
+                query = connection.execute(db.insert(anomalies_table).values(rule_id = 1,frame =result['frame_id'], center_x = obj['relative_coordinates']['center_x'], center_y = obj['relative_coordinates']['center_y'], width = obj['relative_coordinates']['width'], height = obj['relative_coordinates']['height']))
                 io = json.dumps(query.lastrowid)
                 logging.warning(io)
-                query2 = connection.execute(db.insert(video_anomalies_table).values(DetectedAnomalyId = query.lastrowid , VideoId = vid_id))
+                query2 = connection.execute(db.insert(video_anomalies_table).values(detected_anomaly_id = query.lastrowid , video_id = vid_id))
 
     logging.info("Finished Processing " + vid_name)
 
